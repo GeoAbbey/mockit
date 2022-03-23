ba33be99fff3e4da39c81357cf980d675d867ece
gbols <gbolahanpaul@gmail.com>
2022-03-23 11:06:48 +0100

    added filter by date

--- server/modules/dashboard/schema/dashboard.schema.js
+++ server/modules/dashboard/schema/dashboard.schema.js
@@ -1,9 +1,9 @@
 import Joi from "joi";
 
 export const queryOptions = Joi.object().keys({
-  search: Joi.object().keys({
-    assignedLawyerId: Joi.string().guid({ version: "uuidv4" }),
-  }),
+  assignedLawyerId: Joi.string().guid({ version: "uuidv4" }),
+  from: Joi.date().less(Joi.ref("to")),
+  to: Joi.date(),
   paginate: Joi.object().keys({
     page: Joi.number().min(1),
     pageSize: Joi.number().max(20),
